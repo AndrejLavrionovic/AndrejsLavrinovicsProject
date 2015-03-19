607e025812d1c288bb800c634765f34cfa3a41cc
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2015-03-19 11:31:20 +0000

    03 - Program.c Sceleton created (includes, main)

---
+++ AndrejsLavrinovicsProject/Program.c
@@ -0,0 +1,18 @@
+//*****************************************************************************
+// C program that will allow you to retrieve, add, delete, update and
+// manipulate an employee database.
+// The database is password protected and only the correct username and
+// password details will allow the user to gain access to the Employee Menu.
+//*****************************************************************************
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+
+	
+
+	printf("\n\n\n");
+	system("pause");
+}
