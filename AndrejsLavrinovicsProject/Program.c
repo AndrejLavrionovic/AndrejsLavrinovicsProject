9d02d6670a68a62d6451b5249980937b4b887263
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2015-03-19 23:17:30 +0000

    06 - Structures declared.

--- AndrejsLavrinovicsProject/Program.c
+++ AndrejsLavrinovicsProject/Program.c
@@ -8,6 +8,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+// Structures declaration
+struct login{
+	char username[30];
+	char password[20];
+};
+
+struct date{
+	int day;
+	int month;
+	int year;
+};
+
+struct employee{
+	int e_id;
+	char e_name[40];
+	char e_address[100];
+	char e_department[10];
+	struct date e_hiring;
+	float salary;
+	char e_email[40];
+};
+
 void main(void)
 {
 
